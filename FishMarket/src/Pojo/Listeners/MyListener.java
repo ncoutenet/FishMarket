c5cbe13144a3862ae8d40c00965bb3cbb9b096d8
nicolas <nicolas@nicolas-visualisation>
2014-01-23 08:53:40 +0100

    ajout d'une case à cocher pour le mode de l'acheteur et son listener

---
+++ FishMarket/src/Pojo/Listeners/MyListener.java
@@ -0,0 +1,16 @@
+package Pojo.Listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getActionCommand().equals("autobuy")){
+			
+		}
+
+	}
+
+}
