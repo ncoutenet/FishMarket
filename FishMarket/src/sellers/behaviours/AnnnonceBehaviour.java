33147fd7fe5b97d13efac5b9e35fd9e9363da9f3
nicolas <nicolas@nicolas-visualisation>
2014-01-24 10:31:39 +0100

    ajout d'un behaviour d'annonce

---
+++ FishMarket/src/sellers/behaviours/AnnnonceBehaviour.java
@@ -0,0 +1,18 @@
+package sellers.behaviours;
+
+import jade.core.behaviours.Behaviour;
+
+public class AnnnonceBehaviour extends Behaviour {
+
+	@Override
+	public void action() {
+		// TODO passer une annonce au marché
+
+	}
+
+	@Override
+	public boolean done() {
+		return true; // One shot
+	}
+
+}
