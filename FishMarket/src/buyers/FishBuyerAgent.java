884982507bad314c3e10a4c02c6a4a22b74bd934
nicolas <nicolas@nicolas-visualisation>
2014-01-24 16:38:17 +0100

    nettoyage

--- FishMarket/src/buyers/FishBuyerAgent.java
+++ FishMarket/src/buyers/FishBuyerAgent.java
@@ -1,22 +1,13 @@
 package buyers;
 
+import jade.core.AID;
+import jade.core.Agent;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import buyers.behaviours.SearchBehaviour;
 
-import sellers.FishSellerAgent;
-
-import jade.core.AID;
-import jade.core.Agent;
-/*import jade.core.AID;
-import jade.core.behaviours.*;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
-import jade.domain.DFService;
-import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;*/
 
 
 public class FishBuyerAgent extends Agent{
