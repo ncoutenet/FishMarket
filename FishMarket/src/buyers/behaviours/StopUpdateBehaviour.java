ccdeef105ff84bf409dbffd6de0ac0ad2fa40eb6
ncoutenet <ncoutenet@gmail.com>
2014-01-26 12:46:52 +0100

    arret de la mise a jour des vendeurs potentiels

---
+++ FishMarket/src/buyers/behaviours/StopUpdateBehaviour.java
@@ -0,0 +1,36 @@
+package buyers.behaviours;
+
+import java.util.List;
+
+import buyers.FishBuyerAgent;
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+
+public class StopUpdateBehaviour extends Behaviour {
+	private FishBuyerAgent _myAgent;
+
+	public StopUpdateBehaviour(FishBuyerAgent agent) {
+		super();
+		this._myAgent = agent;
+	}
+
+	@Override
+	public void action() {
+		// TODO demander aux vendeur d'arreter les updates
+		ACLMessage stopUpdate = new ACLMessage(ACLMessage.REQUEST);
+		List<AID> senders = this._myAgent.getSellers();
+		for(int i = 0; i < senders.size(); i++){
+			stopUpdate.addReceiver(senders.get(i));
+			stopUpdate.setContent("stopUpdate");
+		}
+		this._myAgent.send(stopUpdate);
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return true; // one shot
+	}
+
+}
